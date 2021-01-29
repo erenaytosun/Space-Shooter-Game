5fb9c4e52d8d0373f2f3330598b18695981f885a
erenaytosun <erenaytosun77@gmail.com>
2021-01-29 18:48:49 +0300

    Space Shooter Game
    Completed

---
+++ Assets/Kodlar/OyunKontrol.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OyunKontrol : MonoBehaviour
+{
+    public GameObject Asteroid;
+    public Vector3 randomPos;
+    int score;
+    public Text text;
+    public Text bitistext;
+    public Text yenidenbasla;
+    bool oyunBittiKontrol = false;
+    bool yenidenBaslaKontol = false;
+
+    void Start()
+    {
+        StartCoroutine(olustur());
+        score = 0;
+        text.text = "Score = 0";
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && yenidenBaslaKontol == true)
+        {
+            SceneManager.LoadScene("level 1");
+        }
+    }
+
+    IEnumerator olustur()
+    {
+        yield return new WaitForSeconds(2);
+
+        while(true)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Vector3 vec = new Vector3(Random.Range(-randomPos.x, randomPos.x), 0, randomPos.z);
+                Instantiate(Asteroid, vec, Quaternion.identity);
+                yield return new WaitForSeconds(0.8f);
+            }
+            yield return new WaitForSeconds(6);
+
+            if (oyunBittiKontrol)
+            {
+                yenidenBaslaKontol = true;
+                break;
+            }
+        }
+        
+    }
+
+    public void scoreArttir(int gelenScore)
+    {
+        score += gelenScore;
+        text.text = "Score = " + score;
+        //Debug.Log(score);
+    }
+
+    public void oyunBitti()
+    {
+        bitistext.text = "Game Over";
+        oyunBittiKontrol = true;
+    }
+}
---
+++ Assets/Kodlar/Rotasyon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotasyon : MonoBehaviour
+{
+    Rigidbody fizik;
+    public float hiz;
+
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+        fizik.angularVelocity = Random.insideUnitSphere * hiz;     /// angularVeloctiy açısal hızdır. Yani cismin rotationunda değişiklik yapar. Random fonk. kullanararak random bir değer de
+        ///dönmesini sağladık. 
+        
+    }
+
+    
+
+    void Update()
+    {
+        
+    }
+}
